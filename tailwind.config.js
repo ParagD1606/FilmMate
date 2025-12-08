badaa5fd52e12240d0eb609b98f6e3680eff5986
ParagD1606 <paragdharamkar2006@gmail.com>
2025-12-08 15:24:17 +0530

    animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: { 
+    extend: {
+      keyframes: {
+        'fade-in-up': {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 1s ease-out forwards',
+        'fade-in': 'fade-in 1s ease-out forwards',
+      },
+    } 
+  },
   plugins: [require('tailwind-scrollbar-hide')],
-
 };
